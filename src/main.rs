5d38586d3a3eca2323fb6abb4d8e274b30a9e7a3
Adam Bricknell <adam_bricknell@live.com>
2023-02-02 11:31:56 +0000

    added priority q

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,122 @@
+use std::time::Instant;
+use rand::{thread_rng, seq::SliceRandom};
+use smallvec::SmallVec;
+use serde::{Deserialize, Serialize};
+
+use std::collections::{BinaryHeap, HashMap};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use nanorand::{Rng, WyRand};
+use std::fmt;
+
+use google_cloud_storage::client::Client;
+use google_cloud_storage::http::objects::download::Range;
+use google_cloud_storage::http::objects::get::GetObjectRequest;
+use google_cloud_storage::http::objects::upload::UploadObjectRequest;
+use google_cloud_storage::http::Error;
+use google_cloud_storage::sign::SignedURLMethod;
+use google_cloud_storage::sign::SignedURLOptions;
+
+use self::priority_queue::PriorityQueueItem;
+
+mod priority_queue;
+
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Debug)]
+struct NodeID(u32);
+
+// implement display options for printing during debug
+impl fmt::Display for NodeID {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug)]
+struct Cost(u16);
+
+impl fmt::Display for Cost {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+struct Edge {
+    to: NodeID,
+    cost: Cost,
+}
+
+
+#[derive(Serialize, Deserialize, Clone)]
+struct GraphWalk {
+    edges_per_node: HashMap<usize, SmallVec<[Edge; 4]>>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct GraphPT {
+    edges_per_node: HashMap<usize, SmallVec<[Edge; 4]>>,
+}
+
+
 fn main() {
     println!("Hello, world!");
+    
 }
 
 fn serialise_input_files() {
 
 }
 
+
+fn json_to_GraphWalk() {
+    
+}
+
+fn json_to_GraphPT() {
+    
+}
+
 fn read_serialised_files() {
     
 }
 
-fn floodfill() {
 
+
+fn floodfill(graph: &Graph, start: NodeID) -> HashMap<NodeID, Cost> {
+
+    let time_limit = Cost(3600);
+
+    let mut queue: BinaryHeap<PriorityQueueItem<Cost, NodeID>> = BinaryHeap::new();
+    queue.push(PriorityQueueItem {
+        cost: Cost(0),
+        value: start,
+    });
+
+    let mut cost_per_node = HashMap::new();
+
+    while let Some(current) = queue.pop() {
+        if cost_per_node.contains_key(&current.value) {
+            continue;
+        }
+        if current.cost > time_limit {
+            continue;
+        }
+        cost_per_node.insert(current.value, current.cost);
+
+        /// got some casting here: could any of it be hurting performance?
+        for edge in &graph.edges_per_node[&(current.value.0 as usize)] {
+            queue.push(PriorityQueueItem {
+                cost: Cost(current.cost.0 + edge.cost.0),
+                value: edge.to,
+            });
+        }
+    }
+
+    cost_per_node
+}
+
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
