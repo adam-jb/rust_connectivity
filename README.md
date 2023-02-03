5a2560d63c4223c8dff520cab07dc5543fe286af
Dustin Carlino <dabreegster@gmail.com>
2023-02-03 13:49:24 +0000

    Clean up project setup instructions / scripts a bit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes on running
+
+Run `./download_input.sh` once to download input data.
---
+++ download_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p data
+cd data
+for x in start_nodes.json init_travel_times.json p1_main_nodes.json \
+	p2_main_nodes.json node_values.json travel_time_relationships.json \
+	subpurpose_purpose_lookup.json number_of_destination_categories.json;
+do
+    wget https://storage.googleapis.com/hack-bucket-8204707942/$x
+done
