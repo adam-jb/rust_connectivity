aa27bb0b3d0b180ccb1e6eb977675a3c17c8a99c
adam-jb <adam.bricknell@dft.gov.uk>
2023-02-10 12:24:57 +0000

    added downloader with curl

---
+++ download_input_mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p data
+mkdir -p serialised_data
+
+cd data
+for x in start_nodes.json init_travel_times.json \
+	padded_node_values_8am.json travel_time_relationships.json \
+    p1_main_nodes_list_8am.json p2_main_nodes_list_8am.json \
+	subpurpose_purpose_lookup.json number_of_destination_categories.json;
+do
+    curl https://storage.googleapis.com/hack-bucket-8204707942/$x > $x
+done
+
