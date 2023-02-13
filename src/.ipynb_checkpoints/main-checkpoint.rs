1ff8313a582e9050d0098a7e49194b7a210dd12d
adam-jb <adam.bricknell@dft.gov.uk>
2023-02-13 10:07:21 +0000

    app now responds to time of day change

--- src/.ipynb_checkpoints/main-checkpoint.rs
+++ src/.ipynb_checkpoints/main-checkpoint.rs
@@ -22,10 +22,13 @@ mod shared;
 // This struct represents state
 struct AppState {
     node_values_1d: Arc<Vec<i32>>,
+    /*
     travel_time_relationships_7: Arc<Vec<i32>>,
     travel_time_relationships_10: Arc<Vec<i32>>,
     travel_time_relationships_16: Arc<Vec<i32>>,
     travel_time_relationships_19: Arc<Vec<i32>>,
+    */
+    travel_time_relationships_all: Arc<Vec<Arc<Vec<i32>>>>,
     subpurpose_purpose_lookup: [i8; 32],
     graph_walk: Arc<Vec<SmallVec<[EdgeWalk; 4]>>>,
     graph_pt: Arc<Vec<SmallVec<[EdgePT; 4]>>>,
@@ -63,20 +66,27 @@ async fn floodfill_pt(data: web::Data<AppState>, input: web::Json<UserInputJSON>
     
     */
     
-    // todo: choose travel_time_relationships. 
-    // could make a vector, then push the relevant travel_time_relationships obj 
-    // eg: &data.travel_time_relationships_7
-    // to the vector
-    // then pass first item of the vector to func below
-
-    println!("started api floodfill");
+    
+    // find which travel time relationships to use
+    let mut time_of_day_ix = 0;
+    if input.trip_start_seconds > 3600 * 10 {
+        time_of_day_ix = 1;
+    }
+    if input.trip_start_seconds > 3600 * 16 {
+        time_of_day_ix = 2;
+    }
+    if input.trip_start_seconds > 3600 * 19 {
+        time_of_day_ix = 3;
+    }
+    
+    println!("started api floodfill\ttime_of_day_ix: {}", time_of_day_ix);
     let mut model_parameters_each_start = Vec::new();
     for i in 0..input.start_nodes_user_input.len() {
         model_parameters_each_start.push((
             &data.graph_walk,
             NodeID(input.start_nodes_user_input[i] as u32),
             &data.node_values_1d,
-            &data.travel_time_relationships_7,
+            &data.travel_time_relationships_all[time_of_day_ix], //&data.travel_time_relationships_7,
             &data.subpurpose_purpose_lookup,
             &data.graph_pt,
             input.trip_start_seconds,
@@ -119,19 +129,32 @@ async fn main() -> std::io::Result<()> {
     let arc_node_values_1d = Arc::new(node_values_1d);
     let arc_graph_walk = Arc::new(graph_walk);
     let arc_graph_pt = Arc::new(graph_pt);
+    
     let arc_travel_time_relationships_7 = Arc::new(travel_time_relationships_7);
     let arc_travel_time_relationships_10 = Arc::new(travel_time_relationships_10);
     let arc_travel_time_relationships_16 = Arc::new(travel_time_relationships_16);
     let arc_travel_time_relationships_19 = Arc::new(travel_time_relationships_19);
+    
+    
+    let travel_time_relationships_all: Vec<Arc<Vec<i32>>> = vec![
+        arc_travel_time_relationships_7,
+        arc_travel_time_relationships_10,
+        arc_travel_time_relationships_16,
+        arc_travel_time_relationships_19
+        ];
+    let arc_travel_time_relationships_all = Arc::new(travel_time_relationships_all);
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
                 node_values_1d: arc_node_values_1d.clone(),
+                travel_time_relationships_all: arc_travel_time_relationships_all.clone(),
+                /*
                 travel_time_relationships_7: arc_travel_time_relationships_7.clone(),
                 travel_time_relationships_10: arc_travel_time_relationships_10.clone(),
                 travel_time_relationships_16: arc_travel_time_relationships_16.clone(),
                 travel_time_relationships_19: arc_travel_time_relationships_19.clone(),
+                */
                 subpurpose_purpose_lookup: subpurpose_purpose_lookup,
                 graph_walk: arc_graph_walk.clone(),
                 graph_pt: arc_graph_pt.clone(),
