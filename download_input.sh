ac93a62d603b4d811e65e4d537987ee36814298f
Dustin Carlino <dabreegster@gmail.com>
2023-02-13 18:07:44 +0000

    Always use curl; dedupe scripts

--- download_input.sh
+++ download_input.sh
@@ -12,6 +12,6 @@ for x in padded_node_values_6am.json travel_time_relationships_10.json \
     p1_main_nodes_list_6am.json p2_main_nodes_list_6am.json \
 	subpurpose_purpose_lookup.json number_of_destination_categories.json;
 do
-    wget https://storage.googleapis.com/hack-bucket-8204707942/$x
+    curl https://storage.googleapis.com/hack-bucket-8204707942/$x > $x
 done
 
